c716d7a554e1128dd1b75b8c9b968ed38cd89faf
Aria-iu <2357648739@qq.com>
2024-03-09 10:55:22 +0800

    change

--- Robot.h
+++ Robot.h
@@ -1,8 +1,12 @@
-#include "Map.h"
+#include "Util.h"
 
 const int robot_num = 10;
+const int Unreachable = 888;
 
-
+enum Get_Or_Not{
+    GET,
+    NOT,
+};
 
 enum Robot_status{
     STOP,
@@ -13,16 +17,20 @@ class Robot
 {
     /* data */
     public:
-        pos Robt_pos;
+        Robt_pos robt_pos;
         int goods;
         Robot_status status;
+        Get_Or_Not is_handle_goods;
+        int berth_num;
     /* method */
         Robot() {}
         Robot(int startx,int starty){
-            Robt_pos.x = startx;
-            Robt_pos.y = starty;
+            robt_pos.x = startx;
+            robt_pos.y = starty;
         }
+        bool can_run();
 };
+
 
 
 
---
+++ Util.h
@@ -0,0 +1,11 @@
+struct Robt_pos {
+    int x, y;
+};
+
+// if Robot is RUN , you can take that
+enum Robot_Direction{
+    W,
+    A,
+    S,
+    D,
+};
