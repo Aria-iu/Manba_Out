100193f5fb90dd026cfcc28e95e6500f866ce7b9
PandaCsd <1480484467@qq.com>
2024-03-09 12:11:01 +0800

    aaa

--- define.h
+++ define.h
@@ -1,9 +1,11 @@
 #ifndef CODECRAFTSDK_DEFINE_H
 #define CODECRAFTSDK_DEFINE_H
 
+#define MAX_FRAME 15000
 #define SIZE 200
 #define NUM_ROBOT 10
 #define NUM_BERTH 1
+
 
 //LEFT  X减小方向
 //RIGHT X增大方向
