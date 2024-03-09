fd3eb129d95e905fc2cd56964dade44914988ab2
Aria-iu <2357648739@qq.com>
2024-03-09 12:17:20 +0800

    change

--- define.h
+++ define.h
@@ -26,6 +26,18 @@ enum Robot_status{
     DUMMY,
 };
 
+struct Robt_pos {
+    int x, y;
+};
+
+// if Robot is RUN , you can take that
+enum Robot_Direction{
+    W,
+    A,
+    S,
+    D,
+};
+
 struct pos {
     int x, y;
 };
